b1ea56292ef16a89f1cfe62047856274cb4c27fd
Hans Fangohr <fangohr@users.noreply.github.com>
2024-10-02 10:53:47 +0100

    add ipython

--- pixi/install-pixi-environment.sh
+++ pixi/install-pixi-environment.sh
@@ -2,6 +2,6 @@
 #
 # 2. to create the environment, navigate to desired installation directory and run:
 pixi init
-pixi add python=3.12 numpy scipy matplotlib sympy pytest jupyter pycodestyle ruff black spyder
+pixi add python=3.12 numpy scipy matplotlib sympy pytest jupyter pycodestyle ruff black spyder ipython pip
 # 3. to activate
 pixi shell
