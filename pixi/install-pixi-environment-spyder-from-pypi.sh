4478dc32c6504cd6f1ee06699996bbed74cc2c2c
Hans Fangohr <fangohr@users.noreply.github.com>
2024-10-02 10:54:10 +0100

    add ipython

--- pixi/install-pixi-environment-spyder-from-pypi.sh
+++ pixi/install-pixi-environment-spyder-from-pypi.sh
@@ -2,7 +2,7 @@
 #
 # 2. to create the environment, navigate to desired installation directory and run:
 pixi init
-pixi add python=3.12 numpy scipy matplotlib sympy pytest jupyter pycodestyle ruff black
+pixi add python=3.12 numpy scipy matplotlib sympy pytest jupyter pycodestyle ruff black ipython pip
 # 3. Install spyder from pypi because the conda-forge package on OSX doesn't work (September 2024):
 pixi add --pypi spyder
 # 4. to activate (and install)
